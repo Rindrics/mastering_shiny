0db7efa36579947350c1671526ffa03cc6740bfe
Akira <smxshxishxad@me.com>
2021-08-11 03:29:23 +0900

    Remove duplication

--- app.R
+++ app.R
@@ -6,13 +6,16 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  output$summary <- renderPrint({
-    dataset <- get(input$dataset, "package:datasets")
-    summary(dataset)
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
   })
+
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+
   output$table <- renderTable({
-    dataset <- get(input$dataset, "package:datasets")
-    dataset
+    dataset()
   })
 }
 shinyApp(ui, server)
