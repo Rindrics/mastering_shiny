9f010886ae86ee4bf14002fd4c8dc0d31b94c0ba
Akira <smxshxishxad@me.com>
2021-08-20 01:36:38 +0900

    :question: Paste code from text

--- app.R
+++ app.R
@@ -1,30 +1,23 @@
 library(shiny)
+library(ggplot2)
 
+datasets <- c("economics", "faithfuld", "seals")
 ui <- fluidPage(
-  sliderInput("x", "If x is", min = 1, max = 50, value = 30),
-  sliderInput("y", "and y is", min = 1, max = 50, value = 5),
-  textOutput("product_plus0"),
-  textOutput("product_plus5"),
-  textOutput("product_plus10")
+  selectInput("dataset", "Dataset", choices = datasets),
+  verbatimTextOutput("summary"),
+  tableOutput("plot")
 )
 
 server <- function(input, output, session) {
-
-  product_plus <- function(conj, z) {
-    renderText({
-        product <- input$x * input$y
-        prodstr <- "(x * y)"
-        if (z == 0) {
-            paste0(conj, prodstr, " is ", product + z)
-        } else {
-            paste0(conj, prodstr, " + ", z, " is ", product + z)
-        }
-    })
-  }
-
-  output$product_plus0 <- product_plus("then, ", 0)
-  output$product_plus5 <- product_plus("and, ", 5)
-  output$product_plus10 <- product_plus("and ", 10)
+  dataset <- reactive({
+    get(input$dataset, "package:ggplot2")
+  })
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  output$plot <- renderPlot({
+    plot(dataset)
+  }, res = 96)
 }
 
 shinyApp(ui, server)
