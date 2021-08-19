a985882eb4d28ea08bdcbd4bf8aa8574effdf238
Akira <smxshxishxad@me.com>
2021-08-20 01:42:49 +0900

    :recycle: Improve app

--- app.R
+++ app.R
@@ -4,19 +4,19 @@ library(ggplot2)
 datasets <- c("economics", "faithfuld", "seals")
 ui <- fluidPage(
   selectInput("dataset", "Dataset", choices = datasets),
-  verbatimTextOutput("summary"),
-  tableOutput("plot")
+  dataTableOutput("summary"),
+  plotOutput("plot")
 )
 
 server <- function(input, output, session) {
   dataset <- reactive({
     get(input$dataset, "package:ggplot2")
   })
-  output$summary <- renderPrint({
-    summary(dataset())
+  output$summary <- renderDataTable({
+    dataset()
   })
   output$plot <- renderPlot({
-    plot(dataset)
+    plot(dataset())
   }, res = 96)
 }
 
