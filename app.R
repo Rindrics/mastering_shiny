f04745377bef28b6053d31005091a2e9ab700a8c
Akira <smxshxishxad@me.com>
2021-08-20 01:26:59 +0900

    :recycle: Generate all text by textOutput()

--- app.R
+++ app.R
@@ -3,23 +3,28 @@ library(shiny)
 ui <- fluidPage(
   sliderInput("x", "If x is", min = 1, max = 50, value = 30),
   sliderInput("y", "and y is", min = 1, max = 50, value = 5),
-  "then, (x * y) is", textOutput("product_plus0"),
-  "and, (x * y) + 5 is", textOutput("product_plus5"),
-  "and (x * y) + 10 is", textOutput("product_plus10")
+  textOutput("product_plus0"),
+  textOutput("product_plus5"),
+  textOutput("product_plus10")
 )
 
 server <- function(input, output, session) {
 
-  product_plus <- function(z) {
+  product_plus <- function(conj, z) {
     renderText({
         product <- input$x * input$y
-        product + z
+        prodstr <- "(x * y)"
+        if (z == 0) {
+            paste0(conj, prodstr, " is ", product + z)
+        } else {
+            paste0(conj, prodstr, " + ", z, " is ", product + z)
+        }
     })
   }
 
-  output$product_plus0 <- product_plus(0)
-  output$product_plus5 <- product_plus(5)
-  output$product_plus10 <- product_plus(10)
+  output$product_plus0 <- product_plus("then, ", 0)
+  output$product_plus5 <- product_plus("and, ", 5)
+  output$product_plus10 <- product_plus("and ", 10)
 }
 
 shinyApp(ui, server)
